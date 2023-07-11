ce8eb0413c0123f86e71e2ed1fc31748b8e75d8e
Hysalim <hysalimapp@gmail.com>
2023-07-11 12:12:58 +0200

    Update main.ts - add cors, validation and plugins

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Create application
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // Configure CORS options
+  app.enableCors({ credentials: true, origin: true });
+  // Add validation and plugins pipes
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(compression());
+  app.use(helmet());
+
+  // Run application
   await app.listen(3000);
 }
 bootstrap();
